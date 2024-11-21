f0d67ab45c90153bb5d6a281c610c6fc5023b8bc
JorgeOteiza <oteiza.jor@gmail.com>
2024-11-21 17:53:20 -0300

    avance en diseños, implementación de nuevos contenedores

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,47 @@
-import React from 'react';
-import '../styles/Footer.css';
+import React from "react";
+import "../styles/Footer.css";
 
 const Footer = () => {
   return (
-    <footer>
-      <section id="contacto" className="contact">
-        <h2>Contacto</h2>
-        <div className="lista-contacto">
-          <p><span>Correo:</span> oteiza.jor@gmail.com</p>
-          <p><span>Teléfono:</span> +569 649-XX-XXX</p>
+    <footer className="footer">
+      <div className="dates-person">
+        <section id="contacto" className="contact">
+          <h2>Contacto</h2>
+          <div className="lista-contacto">
+            <p>
+              <span>Correo:</span> oteiza.jor@gmail.com
+            </p>
+            <p>
+              <span>Teléfono:</span> +569 649-XX-XXX
+            </p>
+            <p>
+              <span>Teléfono:</span> +569 649-XX-XXX
+            </p>
+          </div>
+        </section>
+        <div className="RRSS">
+          <div className="social-links">
+            <a
+              href="https://github.com/JorgeOteiza"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-link"
+            >
+              <i className="fab fa-github"></i> GitHub
+            </a>
+          </div>
+          <div className="social-links">
+            <a
+              href="https://www.linkedin.com/in/jorgecancinoote%C3%ADza/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-link"
+            >
+              <i class="fa-brands fa-linkedin"></i> Linkedin
+            </a>
+          </div>
         </div>
-        <div className="social-links">
-          <a 
-            href="https://github.com/JorgeOteiza" 
-            target="_blank" 
-            rel="noopener noreferrer" 
-            className="social-link"
-          >
-            <i className="fab fa-github"></i> GitHub
-          </a>
-        </div>
-      </section>
-      <p className="footer-note">Jorge Oteiza.</p>
+      </div>
     </footer>
   );
 };
